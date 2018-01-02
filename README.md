c54a5ffbfe141b08948ade9d781434cff0f63957
Jacob Goodall <jacobgoodall@Jacobs-MacBook-Pro.local>
2018-01-02 14:54:12 -0800

    Adding to github

---
+++ README.md
@@ -0,0 +1,6 @@
+# The classic company website
+
+## A website that that has a info and contact page.
+
+Technologies used-
+HTML, CSS, Javascript, node.je, ejs, mongoDB
---
+++ app.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+//app config
+mongoose.connect("mongodb://localhost/form");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Mongoose/model config
+var formSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  email: String,
+  zip: String,
+  state: String,
+  created: {type: Date, default: Date.now}
+});
+var Form = mongoose.model("Form", formSchema)
+
+//Restful Routes
+app.get('/', function(req,res){
+  res.redirect('/home')
+})
+
+app.get('/home', function(req,res){
+  res.render('index')
+});
+
+app.get('/form', function(req,res){
+  res.render('form')
+})
+
+app.post('/form', function(req,res){
+  Form.create(req.body.form, function(err, newForm){
+    if(err){
+      console.log(err)
+    } else {
+      res.redirect('/home')
+    }
+  })
+})
+
+app.listen(process.env.port || 8080, function(){
+  console.log("Running on port 8080")
+})
